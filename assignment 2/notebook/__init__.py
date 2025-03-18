71dd47f948e4c00e95037d9d483e8ec5d10bce87
Emily Ho <yunhsuanemilyho@gmail.com>
2025-03-18 00:01:50 -0400

    Uploaded files

---
+++ assignment 2/notebook/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from .models import db
+from .routes import routes
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.sqlite'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    app.register_blueprint(routes)
+
+    return app
---
+++ assignment 2/notebook/routes.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, request, render_template, redirect, url_for
+from .models import db, Note, Comment
+# from notebook import create_app
+
+routes = Blueprint('routes', __name__)
+
+
+@routes.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        content = request.form['content']
+        success = Note.add(name, content)
+
+        if not success:
+            return 'Fail to add note'
+        
+    all_notes = Note.query.all()
+    term = request.args.get('term', '')
+
+    if term:
+        results = Note.search(term)
+        if not results:
+            message = f'No notes found for "{term}"'
+        else:
+            message = None
+    else:
+        results = []
+        message = None
+
+    return render_template('index.html', all_notes=all_notes, results=results, term=term, message=message)
+
+
+@routes.route('/note')
+def note_detail():
+    name = request.args.get('name')
+    note = Note.query.filter_by(name=name).first()
+    
+    if not note:
+        return 'Note not found', 404
+    
+    comments = Comment.query.filter_by(note_id=note.id).all()
+    return render_template('note.html', name=note.name, content=note.content, date=note.date, comments=comments)
+
+
+@routes.route('/note/<name>/comment', methods=['POST'])
+def add_comment(name):
+    note = Note.query.filter_by(name=name).first()
+    if not note:
+        return 'Note not found', 404
+
+    content = request.form['content']
+    success = Comment.add(note.id, content)
+
+    if not success:
+        return 'Fail to add comment'
+
+    return redirect(url_for('routes.note_detail', name=name))
+
+
+@routes.route('/note/<name>/delete', methods=['POST'])
+def delete_note_and_comments(name):
+    success = Note.delete(name)
+    if not success:
+        return 'Note not found', 404
+
+    return redirect(url_for('routes.index'))
