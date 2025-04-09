b5968643cac61762e1c53994db32d8531b9d102a
Emily Ho <yunhsuanemilyho@gmail.com>
2025-04-08 21:46:41 -0400

    updated files

--- assignment 3/notebook/__init__.py
+++ assignment 3/notebook/__init__.py
@@ -4,8 +4,8 @@ from .routes import routes
 
 
 def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.sqlite'
+    app = Flask(__name__, instance_relative_config=True)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/notes.sqlite'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
